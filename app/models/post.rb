3150d0f3ac58e0a8eb12452636d419e7af30482e
dallinn <dallinn@users.noreply.github.com>
2016-06-09 18:26:56 -0600

    Add Post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
 	has_attached_file :picture, styles: { medium: "500x500>", thumb: "100x100" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 	validates :manufacturer, presence: true
-	validates :model, presence: true
+	validates :model, presence: true, length: { maximum: 30 }
 	validates :price, presence: true
-	validates :body, presence: true
+	validates :body, presence: true, length: { maximum: 1500 }
 	validates :picture, presence: true
 end
