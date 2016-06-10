367bd0fc6a3844713a0c38d97751c9eeb0d041e8
dallinn <dallinn@users.noreply.github.com>
2016-06-09 18:25:28 -0600

    Add seed, modify gitignore

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
 	has_attached_file :picture, styles: { medium: "500x500>", thumb: "100x100" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+	validates :manufacturer, presence: true
+	validates :model, presence: true
+	validates :price, presence: true
+	validates :body, presence: true
+	validates :picture, presence: true
 end
